6d9c9710b5f6692b69d0fec24e23ecaae33c48a4
Ryan Billings <ryanbillings@Ryans-MacBook-Air.local>
2019-12-18 17:13:31 -0800

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+// Variables
+public float maxHealth;
+public float health;
+public float movementSpeed;
+
+private GameObject player;
+
+private bool triggeringPlayer;
+public bool aggro;
+
+public float attackTimer;
+private float _attackTimer;
+private bool attacked;
+
+public float maxDamage;
+public float minDamage;
+public float damage;
+// Functions
+
+
+
+void Start(){
+	player = GameObject.FindWithTag("Player");
+	_attackTimer = attackTimer;
+	health = maxHealth;
+}
+
+void Update(){
+	if(aggro){
+		FollowPlayer( );
+	}
+	
+
+}
+void OnTriggerEnter(Collider other){
+	if(other.tag == "Player"){
+		triggeringPlayer = true;
+	}
+
+}
+
+void OnTriggerExit(Collider other){
+	if(other.tag == "Player"){
+		triggeringPlayer = false;
+	}
+}
+
+public void Attack(){
+	// if(!attacked){
+		damage = Random.Range(minDamage, maxDamage);
+		player.GetComponent<Player>().health -= damage;
+		// attacked = true;
+		// print("Enemy attacked player");
+	// }
+
+}
+
+public void FollowPlayer(){
+	print("engaged");
+	if(!triggeringPlayer){
+		this.transform.position = Vector3.MoveTowards(transform.position,player.transform.position,movementSpeed);
+	}
+	if(_attackTimer  >= 0){
+		attacked = false;
+		_attackTimer = attackTimer;
+
+	}
+
+	if(attacked)
+		_attackTimer -= 1 * Time.deltaTime;
+
+	
+		Attack();
+	}
+
+	
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+//Variables
+
+// Player Vars
+public float maxHealth;
+public float health;
+
+
+public float movementSpeed;
+Animation anim;
+
+public float attackTimer;
+private float currentAttackTimer;
+
+private bool moving;
+private bool attacking;
+private bool followingEnemy;
+
+public float damage;
+public float minDamage;
+public float  maxDamage;
+
+private bool attacked;
+
+
+// pmr
+public GameObject playerMovePoint;
+private Transform pmr;
+private bool triggeringPMR;
+
+
+
+
+// Enemy Vars
+private bool triggeringEnemy;
+private GameObject attackingEnemy;
+
+
+
+// Functions
+void Start(){
+	pmr = Instantiate(playerMovePoint.transform,this.transform.position,Quaternion.identity);
+	pmr.GetComponent<BoxCollider>().enabled = false;
+	anim = GetComponent<Animation>();
+	currentAttackTimer = attackTimer;
+	health = maxHealth;
+}
+void Update(){
+	
+	//Player movement
+
+	Plane playerPlane = new Plane(Vector3.up, transform.position);
+	Ray ray = UnityEngine.Camera.main.ScreenPointToRay(Input.mousePosition);
+	RaycastHit hit;
+	float hitDistance = 0.0f;
+
+	if(playerPlane.Raycast(ray,out hitDistance)){
+		Vector3 mousePosition = ray.GetPoint(hitDistance);
+		if(Input.GetMouseButtonDown(0)){
+			moving = true;
+			triggeringPMR = false; 
+			pmr.transform.position = mousePosition;
+			pmr.GetComponent<BoxCollider>().enabled = true;
+
+			if(Physics.Raycast(ray,out hit))
+			{
+				if(hit.collider.tag == "Enemy"){
+					attackingEnemy = hit.collider.gameObject;
+					followingEnemy = true;
+				}else{
+				attackingEnemy = null;
+				followingEnemy = false;
+			}
+
+			}
+		}
+	}
+
+	
+	if(moving)
+		Move();
+		else
+		{
+			if(attacking)
+				Attack();
+				else
+					Idle();
+		}
+
+		if(triggeringPMR){
+			moving = false;
+		}
+		if(triggeringEnemy)
+			Attack();
+		
+		if(attacked){
+			currentAttackTimer -= 1 * Time.deltaTime;
+		} 
+		if(currentAttackTimer <= 0){
+			currentAttackTimer = attackTimer;
+			attacked = false;
+
+		}
+	}
+
+	public void Idle(){
+		anim.CrossFade("idle");
+	}
+
+
+
+	public void Move(){
+		if(followingEnemy){
+			if(!triggeringEnemy){
+			transform.position = Vector3.MoveTowards(transform.position,attackingEnemy.transform.position, movementSpeed);
+			this.transform.LookAt(attackingEnemy.transform);
+
+			}
+		}else{
+			transform.position = Vector3.MoveTowards(transform.position,pmr.transform.position, movementSpeed);
+			this.transform.LookAt(pmr.transform);
+		}
+
+		anim.CrossFade("walk");
+	}
+
+	public void Attack(){
+		if(!attacked){
+			damage = Random.Range(minDamage,maxDamage);
+		print(damage);
+		attacked = true;
+		}
+
+		if(attackingEnemy){
+			transform.LookAt(attackingEnemy.transform);
+			attackingEnemy.GetComponent<Enemy>().aggro = true;
+		}
+		anim.CrossFade("attack");
+		
+	}
+
+ 
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "PMR"){
+			triggeringPMR = true;
+		}
+		if(other.tag == "Enemy"){
+			triggeringEnemy = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.tag == "PMR"){
+			triggeringPMR = false;
+		}
+		if(other.tag == "Enemy"){
+			triggeringEnemy = false;
+		}
+	}
+	
+}
